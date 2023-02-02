39c6d5b2766d1c113c201ed0ac666e50c1f5cbb8
dreamix-video-editing <124152442+dreamix-video-editing@users.noreply.github.com>
2023-02-02 19:26:27 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -17,30 +17,22 @@ $(document).ready(function() {
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
+//     Loop on each carousel initialized
+//     for(var i = 0; i < carousels.length; i++) {
+//     	// Add listener to  event
+//     	carousels[i].on('before:show', state => {
+// //     		console.log(state);
+//     	});
+//     }
 
     // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
+//     var element = document.querySelector('#my-element');
+//     if (element && element.bulmaCarousel) {
+//     	// bulmaCarousel instance is available as element.bulmaCarousel
+//     	element.bulmaCarousel.on('before-show', function(state) {
+// //     		console.log(state);
+//     	});
+//     }
 
     bulmaSlider.attach();
 
